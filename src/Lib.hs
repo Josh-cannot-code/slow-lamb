7e1345755a79e71c3b7b1a29f910139563372d12
Joshua Dall'Acqua <joshd@localhost.localdomain>
2022-11-04 20:18:19 -0400

    moved lib funcs to lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,62 @@
 module Lib
-    ( someFunc
+    ( halfSpeed
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.WAVE (
+        WAVESamples,
+        WAVESample, 
+        WAVE (WAVE),
+        getWAVEFile,
+        putWAVEFile,
+        waveHeader,
+        waveSamples
+    )
+
+
+
+file :: FilePath
+file = "src/piano2.wav"
+
+out :: FilePath
+out = "src/piano2slow.wav"
+
+iw :: IO WAVE
+iw = getWAVEFile file
+
+iwSamples :: IO WAVESamples
+iwSamples = fmap waveSamples iw
+
+printSamples :: WAVESamples -> IO ()
+printSamples = mapM_ print
+
+printIwSamples :: IO ()
+printIwSamples = printSamples =<< iwSamples
+
+halfSpeed :: WAVESamples -> WAVESamples
+halfSpeed samples = slowHelper samples []
+    where 
+        slowHelper :: WAVESamples -> WAVESamples -> WAVESamples
+        slowHelper s acc = 
+            case s of 
+            [] -> reverse acc 
+            [x] -> slowHelper [] (x:acc)
+            x:y:xs -> slowHelper (y:xs) (avg2 x y:x:acc)
+
+        avg2 :: [WAVESample] -> [WAVESample] -> [WAVESample]
+        avg2 = zipWith (\ x y -> (x `div` 2) + (y `div` 2)) 
+
+processedIw :: IO WAVESamples
+processedIw = fmap halfSpeed iwSamples
+
+printProcessedIw :: IO ()
+printProcessedIw = printSamples . take 5 =<< processedIw
+            
+writeProcessedIw :: IO ()
+writeProcessedIw = do 
+    s <- processedIw
+    h <- fmap waveHeader iw
+    let outWave = WAVE {
+            waveHeader = h,
+            waveSamples = s
+        }
+    putWAVEFile out outWave
